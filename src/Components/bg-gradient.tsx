14f23e4f9b3010d106efc6395dc8ea82fe57c949
thbueno <thiagobueno.s@gmail.com>
2025-11-06 03:48:53 +0700

    delete gradient file

--- src/Components/bg-gradient.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function BgGradient() {
-  return (
-    <div class="spline-container absolute top-0 left-0 w-full h-full -z-10"><iframe src="https://my.spline.design/aidatamodelinteraction-mdTL3FktFVHgDvFr5TKtnYDV" frameborder="0" width="100%" height="100%" id="aura-spline"></iframe></div>
-  )
-}
-
-export default BgGradient
